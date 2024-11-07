7733f2613c2273d2d85e496b78461110e4e7f323
matteo0003 <angoin.matteo@gmail.com>
2024-11-07 22:25:55 +0100

    Update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,3 +5,17 @@ lua54 "yes"
 client_scripts {
     "client/client.lua",
 }
+
+shared_scripts {
+    "@ox_lib/init.lua",
+    "shared/shared.lua",
+}
+
+files {
+    "locales/*.json",
+}
+
+dependencies {
+    "ox_inventory",
+    "ox_target",
+}
